4e77fac7aa7bf122ce23ebd513f1c625ec3141cd
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2024-10-28 19:53:52 +0600

    imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './css/style.css';
 
-import './js/app.js';
-import './js/renderBoard.js';
-import './js/dragAndDrop.js';
-import './js/deleteCard.js';
-import './js/addCard.js';
+import './js/app';
+import './js/renderBoard';
+import './js/dragAndDrop';
+import './js/deleteCard';
+import './js/addCard';
